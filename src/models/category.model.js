a3c6ffd713a4cba7264b2b8f0e45076913879ed8
b477y <mohammedsaleh002@outlook.com>
2025-10-19 02:06:22 +0300

    refactor: updated Category model structure

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,8 +1,22 @@
 import { model, Schema } from "mongoose";
 
-const categorySchama = new Schema({
-  name: String,
-});
+const categorySchama = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: [true, "Category name should be unique"],
+      minLength: [3, "Category name is too short"],
+      maxLength: [32, "Category name is too long"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    image: Sting,
+  },
+  { timestamps: true }
+);
 
 const CatgeoryModel = model("Category", categorySchama);
 
