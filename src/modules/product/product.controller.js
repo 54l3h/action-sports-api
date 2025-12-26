ac176fa5509ef7ac1d3f6a55c53659a3ba17285a
b477y <mohammedsaleh002@outlook.com>
2025-12-26 17:42:51 +0200

    feat(delete-image)

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -14,7 +14,7 @@ import { stripImagesFromBody } from "../../middlewares/stripImages.middleware.js
 const router = Router();
 
 router.delete(
-  "/product/image/:id/",
+  "/product/image/:id",
   authenticationMiddleware,
   authorizationMiddleware(UserRoles.ADMIN),
   productService.deleteImage
